14ce9fa1668b914a50858cd0048f2430cbf09a22
jhughesr <93365883+jhughesr@users.noreply.github.com>
2021-10-29 09:24:59 +0330

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,5 +1,2 @@
-from colabcode import ColabCode
-import os as alpha
-
-alpha.system("ColabCode(port=10000, password="amir1384")")
-
+from colab_ssh import launch_ssh
+launch_ssh('20ASSnrW5Dq7gwwGcFrc4QqJL2z_z48Kovtu435X4x4Jy9ed','amir1384')

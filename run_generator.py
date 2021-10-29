8756a6bd85ad2d8ae871212edb97fc0357c0e49f
jhughesr <93365883+jhughesr@users.noreply.github.com>
2021-10-29 09:09:13 +0330

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,5 +1,4 @@
-pip install colabcode --upgrade
+pip install colab_ssh --upgrade
 
-from colabcode import ColabCode
-
-ColabCode(port=10000, password="amir1384")
+from colab_ssh import launch_ssh
+launch_ssh('20ASSnrW5Dq7gwwGcFrc4QqJL2z_z48Kovtu435X4x4Jy9ed','amir1384')
